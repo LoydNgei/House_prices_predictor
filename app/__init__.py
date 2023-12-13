a555522e70490d6d34ef0063ee2d17ff47c0811b
Loyd Ngei <loydngei@gmail.com>
2023-12-13 19:10:30 +0300

    Flask app

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+# app/__init__.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# Import your views or routes here
+# from app import views
--- app/run.py
+++ app/run.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__);
 
 
 @app.route('/')
-def Home():
+def home():
     return render_template("index.html")
 
 
